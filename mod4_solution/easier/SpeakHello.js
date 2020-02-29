3aab53486583e4d0c1603315f0b76ef9ea573da1
Huang <jhuang8@ebay.com>
2020-02-28 22:56:18 -0800

    add in the mod4 solution

---
+++ mod4_solution/easier/SpeakHello.js
@@ -0,0 +1,13 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+(function (window) {
+
+ var speakWord = "Hello";
+ var helloSpeaker = new Object();
+ helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+ };
+ window.helloSpeaker = helloSpeaker;
+
+})(window);
